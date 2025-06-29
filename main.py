2c54f0710a0b92ce09f34211da5e4aa20ea0b9f6
MurataBlomqvist <adrian.blomqvist1@gmail.com>
2025-06-29 20:59:42 +0900

    adding player sprite and visuals

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import pygame
+import player
 from constants import *
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}") 
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
     pygame.init()
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    sprite = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+	
         pygame.Surface.fill(screen, "black")
 
+        sprite.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
 
 
 if __name__ == "__main__":
