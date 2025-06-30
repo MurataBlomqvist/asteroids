76991c3a198dc85983056b555a0c7c8ba1e9d98b
MurataBlomqvist <adrian.blomqvist1@gmail.com>
2025-06-30 13:38:33 +0900

    Updated game logic
    Started using groups for updating and drawing player

--- main.py
+++ main.py
@@ -11,21 +11,25 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     sprite = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 	    
-        sprite.update(dt)
+        updatable.update(dt)
         pygame.Surface.fill(screen, "black")
+        for obj in drawable:
+            obj.draw(screen)
 
-        sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
-
